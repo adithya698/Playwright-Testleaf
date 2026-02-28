e3c8e5f4b909e714f8d5f90eb78b84951041d6d1
Adithya Subramanian <adithyakaviya@gmail.com>
2026-02-28 20:13:35 +0530

    First commit - Playwright project

---
+++ tests/learnPrimitiveDataType.js
@@ -0,0 +1,14 @@
+var firstName="Adithya"
+console.log(firstName +" "+typeof(firstName));
+
+var companyName="TestLeaf"
+console.log(companyName +" "+typeof(companyName));
+
+var mobileNumber=9988332234
+console.log(mobileNumber +" " +typeof(mobileNumber));
+
+var isAutomation=true
+console.log(isAutomation + " " +typeof(isAutomation));
+
+var hasPlaywrigt
+console.log(hasPlaywrigt +" "+typeof(hasPlaywrigth))
